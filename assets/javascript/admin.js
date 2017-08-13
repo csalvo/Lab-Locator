d9fe678d0259c345bdbf32ed237485811a2b7bd9
Cristina Salvo <csalvo@spendsetter.com>
2017-08-13 01:21:57 -0400

    search functionality

--- assets/javascript/admin.js
+++ assets/javascript/admin.js
@@ -18,5 +18,43 @@ database.ref("/labList/").on("child_added", function(snapshot){
               '</td><td><i class="fa fa-pencil" aria-hidden="true"></i></td><td><i class="fa fa-times-circle-o" aria-hidden="true"></i></td></tr><hr>';
 
  $("#lab-table tbody").append(row);
- console.log(snapshot.val());
+
 });
+
+
+$("#searchButton").on("click", function() {
+
+var orderBy = $("#keyFilter").val();
+var search = $("#searchBox").val();
+console.log(search);
+$('#lab-table tbody').empty();
+
+var ref = database.ref("labList");
+ref.orderByChild(orderBy).equalTo(search).on("child_added", function(snapshot) {
+  console.log(snapshot.val());
+
+    row = '<tr><td>' + snapshot.val().labName + '</td><td>' + snapshot.val().address + ", " + snapshot.val().city +
+ 			", " + snapshot.val().state + ", " + snapshot.val().zip + 
+              '</td><td>'+ snapshot.val().partnersAffiliate + '</td><td>' + snapshot.val().labOrders + '</td><td>' + snapshot.val().phone + '</td><td>' + snapshot.val().fax + 
+              '</td><td><i class="fa fa-pencil" aria-hidden="true"></i></td><td><i class="fa fa-times-circle-o" aria-hidden="true"></i></td></tr><hr>';
+
+ $("#lab-table tbody").append(row);
+});
+});
+
+
+$("#clearButton").on("click",function(){
+	$('#lab-table tbody').empty();
+
+	database.ref("/labList/").on("child_added", function(snapshot){ 
+ var row = '<tr><td>' + snapshot.val().labName + '</td><td>' + snapshot.val().address + ", " + snapshot.val().city +
+ 			", " + snapshot.val().state + ", " + snapshot.val().zip + 
+              '</td><td>'+ snapshot.val().partnersAffiliate + '</td><td>' + snapshot.val().labOrders + '</td><td>' + snapshot.val().phone + '</td><td>' + snapshot.val().fax + 
+              '</td><td><i class="fa fa-pencil" aria-hidden="true"></i></td><td><i class="fa fa-times-circle-o" aria-hidden="true"></i></td></tr><hr>';
+
+ $("#lab-table tbody").append(row);
+});
+});
+
+
+
