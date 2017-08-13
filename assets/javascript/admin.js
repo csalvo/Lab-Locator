b3b1b17f33d71f0d0725f1d21f09160256447316
Cristina Salvo <csalvo@spendsetter.com>
2017-08-13 12:05:30 -0400

    adding search validation & clear button

--- assets/javascript/admin.js
+++ assets/javascript/admin.js
@@ -10,54 +10,60 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-
-database.ref("/labList/").on("child_added", function(snapshot) {
-    var row = '<tr><td>' + snapshot.val().labName + '</td><td>' + snapshot.val().address + ", " + snapshot.val().city +
-        ", " + snapshot.val().state + ", " + snapshot.val().zip +
-        '</td><td>' + snapshot.val().partnersAffiliate + '</td><td>' + snapshot.val().labOrders + '</td><td>' + snapshot.val().phone + '</td><td>' + snapshot.val().fax +
-        '</td><td><i class="fa fa-pencil" aria-hidden="true"></i></td><td><i class="fa fa-times-circle-o" aria-hidden="true"></i></td></tr><hr>';
-
-    $("#lab-table tbody").append(row);
-
-});
-
+loadLabs();
 
 $("#searchButton").on("click", function() {
     var searchFor = $("#searchBox").val();
-    searchLabs(searchFor);
+    if (searchFor != ""){
+    	searchLabs(searchFor);
+    } else{
+    	$("#search").append("<div id='searchValidation'>Please enter a search term</div>");
+    }
+    
 });
 
 $("#clearButton").on("click", function() {
     clearSearch();
 });
 
-function searchLabs(searchTerm){
+function searchLabs(searchTerm) {
+
     $('#lab-table tbody').empty();
     searchArray = [];
     var ref = database.ref("labList");
     ref.on("child_added", function(snapshot) {
-    	searchArray.push(snapshot.val())
+        searchArray.push(snapshot.val())
     });
-    res2 = JSON.search( searchArray, "//*[contains(text(),'" + searchTerm + "')]/.." );
+    currentSearch = JSON.search(searchArray, "//*[contains(text(),'" + searchTerm + "')]/..");
 
-	for (var i = 0; i < res2.length; i++) {
-		row = '<tr><td>' + res2[i].labName + '</td><td>' + res2[i].address + ", " + 
-		res2[i].city + ", " + res2[i].state + ", " + res2[i].zip + '</td><td>' + 
-		res2[i].partnersAffiliate + '</td><td>' + res2[i].labOrders + '</td><td>' + 
-        res2[i].phone + '</td><td>' + res2[i].fax + '</td><td><i class="fa fa-pencil" aria-hidden="true"></i></td><td><i class="fa fa-times-circle-o" aria-hidden="true"></i></td></tr><hr>';
-		$("#lab-table tbody").append(row);
-	}
+    if (currentSearch.length > 0) {
+        for (var i = 0; i < currentSearch.length; i++) {
+            row = '<tr><td>' + currentSearch[i].labName + '</td><td>' + currentSearch[i].address + ", " +
+                currentSearch[i].city + ", " + currentSearch[i].state + ", " + currentSearch[i].zip + '</td><td>' +
+                currentSearch[i].partnersAffiliate + '</td><td>' + currentSearch[i].labOrders + '</td><td>' +
+                currentSearch[i].phone + '</td><td>' + currentSearch[i].fax + '</td><td><i class="fa fa-pencil" aria-hidden="true"></i></td><td><i class="fa fa-times-circle-o" aria-hidden="true"></i></td></tr><hr>';
+            $("#lab-table tbody").append(row);
+        }
+
+    } else if (currentSearch <= 0) {
+        row = '<tr><td>No labs found. Try a new search.</td><td>'
+        $("#lab-table tbody").append(row);
+    } 
 }
 
-function clearSearch(){
-	$('#lab-table tbody').empty();
+function clearSearch() {
+    $('#lab-table tbody').empty();
+    $("#searchBox").val("");
+    loadLabs();
+}
 
-    database.ref("/labList/").on("child_added", function(snapshot) {
+function loadLabs() {
+    database.ref("/labList/").orderByChild("/labName").on("child_added", function(snapshot) {
         var row = '<tr><td>' + snapshot.val().labName + '</td><td>' + snapshot.val().address + ", " + snapshot.val().city +
             ", " + snapshot.val().state + ", " + snapshot.val().zip +
             '</td><td>' + snapshot.val().partnersAffiliate + '</td><td>' + snapshot.val().labOrders + '</td><td>' + snapshot.val().phone + '</td><td>' + snapshot.val().fax +
             '</td><td><i class="fa fa-pencil" aria-hidden="true"></i></td><td><i class="fa fa-times-circle-o" aria-hidden="true"></i></td></tr><hr>';
 
         $("#lab-table tbody").append(row);
-    })
-};
+    });
+}
