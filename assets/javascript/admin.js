72de75513777655382179cf1b3d6ae5be881d8b8
Cristina Salvo <csalvo@spendsetter.com>
2017-08-13 11:19:24 -0400

    make the search function great again

--- assets/javascript/admin.js
+++ assets/javascript/admin.js
@@ -5,56 +5,59 @@ var config = {
     projectId: "lab-locator",
     storageBucket: "lab-locator.appspot.com",
     messagingSenderId: "609841097386"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
 var database = firebase.database();
 
 
-database.ref("/labList/").on("child_added", function(snapshot){ 
- var row = '<tr><td>' + snapshot.val().labName + '</td><td>' + snapshot.val().address + ", " + snapshot.val().city +
- 			", " + snapshot.val().state + ", " + snapshot.val().zip + 
-              '</td><td>'+ snapshot.val().partnersAffiliate + '</td><td>' + snapshot.val().labOrders + '</td><td>' + snapshot.val().phone + '</td><td>' + snapshot.val().fax + 
-              '</td><td><i class="fa fa-pencil" aria-hidden="true"></i></td><td><i class="fa fa-times-circle-o" aria-hidden="true"></i></td></tr><hr>';
+database.ref("/labList/").on("child_added", function(snapshot) {
+    var row = '<tr><td>' + snapshot.val().labName + '</td><td>' + snapshot.val().address + ", " + snapshot.val().city +
+        ", " + snapshot.val().state + ", " + snapshot.val().zip +
+        '</td><td>' + snapshot.val().partnersAffiliate + '</td><td>' + snapshot.val().labOrders + '</td><td>' + snapshot.val().phone + '</td><td>' + snapshot.val().fax +
+        '</td><td><i class="fa fa-pencil" aria-hidden="true"></i></td><td><i class="fa fa-times-circle-o" aria-hidden="true"></i></td></tr><hr>';
 
- $("#lab-table tbody").append(row);
+    $("#lab-table tbody").append(row);
 
 });
 
 
 $("#searchButton").on("click", function() {
-
-var orderBy = $("#keyFilter").val();
-var search = $("#searchBox").val();
-console.log(search);
-$('#lab-table tbody').empty();
-
-var ref = database.ref("labList");
-ref.orderByChild(orderBy).equalTo(search).on("child_added", function(snapshot) {
-  console.log(snapshot.val());
-
-    row = '<tr><td>' + snapshot.val().labName + '</td><td>' + snapshot.val().address + ", " + snapshot.val().city +
- 			", " + snapshot.val().state + ", " + snapshot.val().zip + 
-              '</td><td>'+ snapshot.val().partnersAffiliate + '</td><td>' + snapshot.val().labOrders + '</td><td>' + snapshot.val().phone + '</td><td>' + snapshot.val().fax + 
-              '</td><td><i class="fa fa-pencil" aria-hidden="true"></i></td><td><i class="fa fa-times-circle-o" aria-hidden="true"></i></td></tr><hr>';
-
- $("#lab-table tbody").append(row);
-});
+    var searchFor = $("#searchBox").val();
+    searchLabs(searchFor);
 });
 
+$("#clearButton").on("click", function() {
+    clearSearch();
+});
 
-$("#clearButton").on("click",function(){
+function searchLabs(searchTerm){
+    $('#lab-table tbody').empty();
+    searchArray = [];
+    var ref = database.ref("labList");
+    ref.on("child_added", function(snapshot) {
+    	searchArray.push(snapshot.val())
+    });
+    res2 = JSON.search( searchArray, "//*[contains(text(),'" + searchTerm + "')]/.." );
+
+	for (var i = 0; i < res2.length; i++) {
+		row = '<tr><td>' + res2[i].labName + '</td><td>' + res2[i].address + ", " + 
+		res2[i].city + ", " + res2[i].state + ", " + res2[i].zip + '</td><td>' + 
+		res2[i].partnersAffiliate + '</td><td>' + res2[i].labOrders + '</td><td>' + 
+        res2[i].phone + '</td><td>' + res2[i].fax + '</td><td><i class="fa fa-pencil" aria-hidden="true"></i></td><td><i class="fa fa-times-circle-o" aria-hidden="true"></i></td></tr><hr>';
+		$("#lab-table tbody").append(row);
+	}
+}
+
+function clearSearch(){
 	$('#lab-table tbody').empty();
 
-	database.ref("/labList/").on("child_added", function(snapshot){ 
- var row = '<tr><td>' + snapshot.val().labName + '</td><td>' + snapshot.val().address + ", " + snapshot.val().city +
- 			", " + snapshot.val().state + ", " + snapshot.val().zip + 
-              '</td><td>'+ snapshot.val().partnersAffiliate + '</td><td>' + snapshot.val().labOrders + '</td><td>' + snapshot.val().phone + '</td><td>' + snapshot.val().fax + 
-              '</td><td><i class="fa fa-pencil" aria-hidden="true"></i></td><td><i class="fa fa-times-circle-o" aria-hidden="true"></i></td></tr><hr>';
+    database.ref("/labList/").on("child_added", function(snapshot) {
+        var row = '<tr><td>' + snapshot.val().labName + '</td><td>' + snapshot.val().address + ", " + snapshot.val().city +
+            ", " + snapshot.val().state + ", " + snapshot.val().zip +
+            '</td><td>' + snapshot.val().partnersAffiliate + '</td><td>' + snapshot.val().labOrders + '</td><td>' + snapshot.val().phone + '</td><td>' + snapshot.val().fax +
+            '</td><td><i class="fa fa-pencil" aria-hidden="true"></i></td><td><i class="fa fa-times-circle-o" aria-hidden="true"></i></td></tr><hr>';
 
- $("#lab-table tbody").append(row);
-});
-});
-
-
-
+        $("#lab-table tbody").append(row);
+    })
+};
