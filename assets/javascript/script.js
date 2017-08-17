94174b24bae0aa4206e590375251b55fc21be308
awallathome <adam.wallis@me.com>
2017-08-16 23:24:08 -0400

    final fixes for admin authorization

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,4 @@
-function adminChanges(){
+
 
 var config = {
     apiKey: "AIzaSyBp0NeXcn_s0KN5Fk5GzKFrVUROXVPfwFY",
@@ -11,49 +11,73 @@ var config = {
 
 firebase.initializeApp(config);
 
-var adminEmail = $("#adminEmail");
-var password = $("#enterPw");
-var btnLogin = $("#btnLogin");
-var btnSignUp = $("#btnSignup");
-var btnLogout = $("#btnLogout");
-
-$("#btnLogin").click(function authAdmin(){
- 
-  var email = adminEmail.val();
-  var pass = password.val();
-  var auth = firebase.auth();
-  firebase.auth().signInWithEmailAndPassword(email, pass).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-});
-  
-  if (firebase.auth()) {
-      console.log("yep");
-      window.open("admin.html");
-  } else {
-      console.log("nope");
-  }
-  
-});
-
-$("#btnSignUp").click(function(){
-  var email = adminEmail.val();
-  var pass = password.val();
-  var auth = firebase.auth();
-  var promise = auth.createUserWithEmailAndPassword(email, pass);
-});
-
-$("#btnLogout").click( function(){
-    firebase.auth().signOut().then(function() {
-  // Sign-out successful.
-  }).catch(function(error) {
-  // An error happened.
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE)
+  .then(function() {
+    // Existing and future Auth states are now persisted in the current
+    // session only. Closing the window would clear any existing state even
+    // if a user forgets to sign out.
+    // ...
+    // New sign-in will be persisted with session persistence.
+    return firebase.auth().signInWithEmailAndPassword(email, password);
+  })
+  .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
   });
+
+var user = firebase.auth().currentUser;
+  
+  $("#btnLogin").click(function authAdmin(){
+    var email = $("#adminEmail").val();
+    var pass = $("#enterPw").val();
+    $("#adminEmail").val("");
+    $("#enterPw").val("");
+    var auth = firebase.auth();
+    var user = firebase.auth().currentUser;
+    firebase.auth().signInWithEmailAndPassword(email, pass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+      // ... 
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user != null) {
+        console.log("yep");
+        window.open("admin.html");
+        location.reload();
+      } else {
+        console.log("nope");
+      }
+    });
+});
+  
+
 });
 
-};
+//  $("#btnSignUp").click(function(){
+//    var email = $("#adminEmail").val();
+//    var pass = $("#enterPw").val();
+//    $("#adminEmail").val("");
+//    $("#enterPw").val("");
+//    var currentUser = firebase.auth().currentUser;
+//    firebase.auth().createUserWithEmailAndPassword(email, pass);
+//    if (firebase.auth(currentUser) == true){
+//        console.log('created');
+//      } else {
+//        console.log('try again');
+//      }
+//  });
 
-adminChanges();
+  $("#btnLogout").click(function(){
+    console.log("ok");  
+    var user = firebase.auth().currentUser;
+    firebase.auth().signOut().then(function() {
+    }).catch(function(error) {
+    // An error happened.
+    });
+    window.close();
+  });
+
+
+
 
